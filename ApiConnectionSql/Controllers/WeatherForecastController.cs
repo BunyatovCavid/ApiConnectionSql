a51c777a019885afc0c2a89cfdebb5aa8fade08f
bunyatovcavid2003@gmail.com <bunyatovcavid2003@gmail.com>
2023-02-14 15:12:48 +0400

    Add project files.

---
+++ ApiConnectionSql/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using ApiConnectionSql.code;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace ApiConnectionSql.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+           var returnvalue =JsonConvert.SerializeObject(Connect.Get());
+            return returnvalue;
+        }
+
+        [HttpPost]
+        public string Post([FromQuery] string name, string life)
+        {
+            Connect.Post(name, life);
+            var returnvalue = JsonConvert.SerializeObject(Connect.Get());
+            return returnvalue;
+        }
+
+        [HttpPut]
+        public string Put([FromQuery] int id,string name, string life)
+        {
+            Connect.Put(id,name,life);
+            var returnvalue = JsonConvert.SerializeObject(Connect.Get());
+            return returnvalue;
+        }
+
+        [HttpDelete]
+        public string Delete([FromQuery] int id)
+        {
+            Connect.Delete(id);
+            var returnvalue = JsonConvert.SerializeObject(Connect.Get());
+            return returnvalue;
+        }
+    }
+}
---
+++ ApiConnectionSql/code/Connect.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Xml.Linq;
+
+namespace ApiConnectionSql.code
+{
+    public class Connect
+    {
+        static public DataTable Get()
+        { 
+            using (SqlConnection connection = new SqlConnection("Data Source=WIN-PFGV5N8DK24;Initial Catalog=ApiConnect;Integrated Security=True"))
+            {
+
+                using (SqlCommand cmd1 = new SqlCommand("ViewPeople", connection))
+                {
+                    cmd1.CommandType = CommandType.StoredProcedure;
+
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+
+                    using (SqlDataAdapter da = new SqlDataAdapter())
+                    {
+                        da.SelectCommand = cmd1;
+                        DataTable table = new DataTable();
+                        da.Fill(table);
+                        if (connection.State == ConnectionState.Open)
+                            connection.Close();
+                        return table;
+                    }
+                }
+
+            }
+        }
+
+        static public void Post(string name,string life)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=WIN-PFGV5N8DK24;Initial Catalog=ApiConnect;Integrated Security=True"))
+            {
+
+                using (SqlCommand cmd1 = new SqlCommand("InsertPerson", connection))
+                {
+                    cmd1.CommandType = CommandType.StoredProcedure;
+
+                    cmd1.Parameters.AddWithValue("@name", name);
+                    cmd1.Parameters.AddWithValue("@life", life);
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+                    cmd1.ExecuteNonQuery();
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+
+            }
+        }
+        static public void Put(int id, string name, string life)
+        {
+
+            using (SqlConnection connection = new SqlConnection("Data Source=WIN-PFGV5N8DK24;Initial Catalog=ApiConnect;Integrated Security=True"))
+            {
+
+                using (SqlCommand cmd1 = new SqlCommand("UpdatePerson", connection))
+                {
+                    cmd1.CommandType = CommandType.StoredProcedure;
+
+                    cmd1.Parameters.AddWithValue("@id", id);
+                    cmd1.Parameters.AddWithValue("@name", name);
+                    cmd1.Parameters.AddWithValue("@life", life);
+
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+                    cmd1.ExecuteNonQuery();
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+
+            }
+        }
+        static public void Delete(int id)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=WIN-PFGV5N8DK24;Initial Catalog=ApiConnect;Integrated Security=True"))
+            {
+
+                using (SqlCommand cmd1 = new SqlCommand("DeletePerson", connection))
+                {
+                    cmd1.CommandType = CommandType.StoredProcedure;
+
+                    cmd1.Parameters.AddWithValue("@id", id);
+
+                    if (connection.State != ConnectionState.Open)
+                        connection.Open();
+                    cmd1.ExecuteNonQuery();
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+
+            }
+        }
+    }
+}
